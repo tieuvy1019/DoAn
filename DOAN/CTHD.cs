329f62aec9d01704186581c02a8aa0a21fef61eb
Vy Tran <tieuvy1234560@gmail.com>
2021-01-08 09:14:26 +0000

    up

--- DOAN/CTHD.cs
+++ DOAN/CTHD.cs
@@ -1,10 +1,7 @@
 namespace DOAN
 {
-    using System;
-    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
 
     [Table("CTHD")]
     public partial class CTHD
--- DOAN/Login.Designer.cs
+++ DOAN/Login.Designer.cs
@@ -40,17 +40,20 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(448, 220);
+            this.textBox1.Location = new System.Drawing.Point(336, 179);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(125, 34);
+            this.textBox1.Size = new System.Drawing.Size(95, 28);
             this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(448, 260);
+            this.textBox2.Location = new System.Drawing.Point(336, 211);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(125, 34);
+            this.textBox2.Size = new System.Drawing.Size(95, 28);
             this.textBox2.TabIndex = 2;
             this.textBox2.UseSystemPasswordChar = true;
             // 
@@ -59,9 +62,10 @@
             this.button1.BackColor = System.Drawing.Color.Brown;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button1.Location = new System.Drawing.Point(473, 323);
+            this.button1.Location = new System.Drawing.Point(355, 262);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(117, 46);
+            this.button1.Size = new System.Drawing.Size(88, 37);
             this.button1.TabIndex = 4;
             this.button1.Text = "LOGIN";
             this.button1.UseVisualStyleBackColor = false;
@@ -70,9 +74,10 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::DOAN.Properties.Resources._23992796_login_icon_or_button_login_login_button_login_icon_login_sign_icon_button_sign_sign_in_word_text_red;
-            this.pictureBox2.Location = new System.Drawing.Point(404, 309);
+            this.pictureBox2.Location = new System.Drawing.Point(303, 251);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(200, 73);
+            this.pictureBox2.Size = new System.Drawing.Size(150, 59);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 3;
             this.pictureBox2.TabStop = false;
@@ -80,9 +85,10 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::DOAN.Properties.Resources._1_fy_FmjARwkGASSQM9Va89w;
-            this.pictureBox1.Location = new System.Drawing.Point(1, 5);
+            this.pictureBox1.Location = new System.Drawing.Point(1, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(798, 441);
+            this.pictureBox1.Size = new System.Drawing.Size(598, 358);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -90,15 +96,16 @@
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Login";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
